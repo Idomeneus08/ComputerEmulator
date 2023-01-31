2a20833b3e099f260185d750e3bd219a8ac0fcad
Talen <Talen.Chadburn@outlook.com>
2023-01-31 14:24:25 -0700

    Added "Subtract" with opcode 000010 Added "Subtract Unsigned" with opcode 000011

--- ComputerEmulator/centralProcessing.h
+++ ComputerEmulator/centralProcessing.h
@@ -58,8 +58,21 @@ class CPU
 
 					m_registers[destinationRegister] = (unsigned int)m_registers[register2] + (unsigned int)m_registers[register1];
 
-				case 2:
+				case 2:		//subtract
 
+					int register1 = convertBinary(s, 6, 5);
+					int register2 = convertBinary(s, 11, 5);
+					int destinationRegister = convertBinary(s, 16, 5);
+
+					m_registers[destinationRegister] = m_registers[register2] - m_registers[register1];
+
+				case 3:		//subtract unsigned
+
+					int register1 = convertBinary(s, 6, 5);
+					int register2 = convertBinary(s, 11, 5);
+					int destinationRegister = convertBinary(s, 16, 5);
+
+					m_registers[destinationRegister] = abs(m_registers[register2] - m_registers[register1]);
 
 			}
 			
